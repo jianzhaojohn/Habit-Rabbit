ae1374aa64dc0081de57cb712cb045e3819140fc
sunnyxu715 <xian1995@gmail.com>
2018-02-20 12:18:57 -0500

    update profile

---
+++ app/src/main/java/comjianzhaojohnhabit_rabbit/httpsgithub/habit_rabbit/ProfileActivity.java
@@ -0,0 +1,13 @@
+package comjianzhaojohnhabit_rabbit.httpsgithub.habit_rabbit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
