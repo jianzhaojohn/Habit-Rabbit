afb923a9d48b3818db758698171d9a8c8ce8df56
Jonathan Zeng <jzeng4@buffalo.edu>
2018-03-12 03:00:03 -0400

    Bug fixes, icon change, app name change

--- app/src/main/java/comjianzhaojohnhabit_rabbit/httpsgithub/habit_rabbit/CalendarActivity.java
+++ app/src/main/java/comjianzhaojohnhabit_rabbit/httpsgithub/habit_rabbit/CalendarActivity.java
@@ -1,56 +1,28 @@
 package comjianzhaojohnhabit_rabbit.httpsgithub.habit_rabbit;
 
-import android.content.Context;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.CalendarView;
-import android.widget.DatePicker;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import org.w3c.dom.Text;
-
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
 
 public class CalendarActivity extends AppCompatActivity {
+
+    private CalendarView mCalendarView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
-
-        DatePicker dp=(DatePicker)findViewById(R.id.datePicker);
-        final TextView eventview=(TextView)findViewById(R.id.textView5);
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTimeInMillis(System.currentTimeMillis());
-        dp.init(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH), new DatePicker.OnDateChangedListener() {
-            @Override
-            public void onDateChanged(DatePicker datePicker, int year, int month, int day) {
-                eventview.setText("currently, you dont have any event in this day");
-
-            }
-        });
-
-
-
-
-
+        mCalendarView = (CalendarView) findViewById(R.id.calendarView);
     }
-
 
     // jump to habits list view
     public void gotoHabit(View view) {
         Intent intent = new Intent(CalendarActivity.this, HabitListActivity.class);
         CalendarActivity.this.startActivity(intent);
     }
-
-
 
 
     //need code for fetching habit data locally or from server
