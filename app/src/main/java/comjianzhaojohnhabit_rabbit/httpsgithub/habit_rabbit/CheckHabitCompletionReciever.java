913f6125ea670f27c4da6cc4184626d4c2aa4a1d
Alison Rogers <arogers7@buffalo.edu>
2018-03-27 00:20:23 -0400

    daily alarm started at boot

---
+++ app/src/main/java/comjianzhaojohnhabit_rabbit/httpsgithub/habit_rabbit/CheckHabitCompletionReciever.java
@@ -0,0 +1,15 @@
+package comjianzhaojohnhabit_rabbit.httpsgithub.habit_rabbit;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class CheckHabitCompletionReciever extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
