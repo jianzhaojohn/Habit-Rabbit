c5f60be654097fe89bb68d3d72867836917b7be1
Xiaoyun Ding <xiaoyunding0122@gmail.com>
2018-04-20 02:34:15 -0400

    decorated navigation view

--- app/src/main/java/comjianzhaojohnhabit_rabbit/httpsgithub/habit_rabbit/MainActivity.java
+++ app/src/main/java/comjianzhaojohnhabit_rabbit/httpsgithub/habit_rabbit/MainActivity.java
@@ -11,6 +11,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import java.io.File;
 import java.text.SimpleDateFormat;
@@ -43,7 +44,11 @@ public class MainActivity extends AppCompatActivity
         navigationView.setNavigationItemSelectedListener(this);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-
+        NavigationView navView = (NavigationView) findViewById(R.id.nav_view);
+        navView.setNavigationItemSelectedListener(this);
+        View header=navigationView.getHeaderView(0);
+        TextView username = header.findViewById(R.id.user);
+        username.setText(HabitList.getUserName());
     }
 
     public static boolean isRabbitAlive() {
@@ -108,27 +113,32 @@ public class MainActivity extends AppCompatActivity
     }
 
 
+/*
 
 
-    /**
+    */
+/**
      *Initialize the contents of the Activity's standard options menu
      *
      * @param menu-The options menu in which you place your items
      * @return You must return true for the menu to be displayed; if you return false it will not be shown.
-     */
+     *//*
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.navigation_menu, menu);
         return true;
     }
 
-    /**
+    */
+/**
      *This hook is called whenever an item in your options menu is selected.
      * You can use this method for any items for which you would like to do processing without those other facilities.
      *
      * @param item- The menu item that was selected.
      * @return boolean Return false to allow normal menu processing to proceed, true to consume it here.
-     */
+     *//*
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (mToggle.onOptionsItemSelected(item)) {
@@ -165,6 +175,7 @@ public class MainActivity extends AppCompatActivity
         }
         return super.onOptionsItemSelected(item);
     }
+*/
 
     /**
      * same as above function but this one is for naviagation bar
@@ -190,7 +201,10 @@ public class MainActivity extends AppCompatActivity
                     String fileName = SharedPref.FILE_NAME;
                     File file= new File(this.getFilesDir().getParent()+"/shared_prefs/"+fileName+".xml");
                     file.delete();
-                    startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                    //open the login page
+                    Intent intent = new Intent(this, LoginActivity.class);
+                    intent.setFlags(intent.FLAG_ACTIVITY_NEW_TASK | intent.FLAG_ACTIVITY_CLEAR_TOP | intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
                 }catch(Exception e){}
                 break;
             default:
